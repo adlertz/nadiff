f2700030f7ae550d8310aeb3cf87b1b93357267b
Niclas Adlertz <adlertz@gmail.com>
2020-04-23 16:48:34 +0200

    parse: Add initial support for binary files

--- parse.c
+++ parse.c
@@ -288,6 +288,29 @@ is_dissimiliarity_index_line(struct line * l)
         && is_char_at_idx(l, i++, 'x');
 }
 
+static bool
+is_binary_file(struct line * l)
+{
+    if (l->data == NULL)
+        return false;
+
+    /* TODO Verify that this is the only format of Binary file output. Also add filename checks */
+    unsigned i = 0;
+    return is_char_at_idx(l, i++, 'B')
+        && is_char_at_idx(l, i++, 'i')
+        && is_char_at_idx(l, i++, 'n')
+        && is_char_at_idx(l, i++, 'a')
+        && is_char_at_idx(l, i++, 'r')
+        && is_char_at_idx(l, i++, 'y')
+        && is_char_at_idx(l, i++, ' ')
+        && is_char_at_idx(l, i++, 'f')
+        && is_char_at_idx(l, i++, 'i')
+        && is_char_at_idx(l, i++, 'l')
+        && is_char_at_idx(l, i++, 'e')
+        && is_char_at_idx(l, i++, 's')
+        && is_char_at_idx(l, i++, ' ');
+}
+
 /*
  * One more more extended header lines:
  * old mode <mode>
@@ -339,8 +362,14 @@ read_extended_header_lines(struct diff * d)
                 return false;
             }
         } else if (read_index_line(l)) {
-            d->expect_line_changes = true;
             /* expect this extended header to be last */
+            l = stdin_read_line();
+
+            /* if binary file then we don't to anything else */
+            if (!is_binary_file(l)) {
+                d->expect_line_changes = true;
+                stdin_reset_cur_line();
+            }
             return true;
         } else {
             /* found a line which is not an extended header line */
